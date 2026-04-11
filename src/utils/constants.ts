3afe22997024de79b42535fc9a168afe37554af8
Brian Stasiukaitis <brianstas@gmail.com>
2026-04-10 22:22:28 -0400

    done deal
    got it out of the way

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -39,6 +39,8 @@ export const COLORS = {
   HP_BAR_BG: 0x2c3e50,
 } as const;
 
+// one of these days, this will be my priority
+
 // Movement configuration
 export const MOVEMENT = {
   SPEED: 200, // Milliseconds per tile movement
